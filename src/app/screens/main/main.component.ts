fd66075e7a6648223d0c189065998576bf527777
Franklin <franklinbarbs763@gmail.com>
2025-01-27 17:59:54 -0300

    feat(delayDigitation): should add new component to create typing delay effect

--- src/app/screens/main/main.component.ts
+++ src/app/screens/main/main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FormNewCelebrationComponent } from './components/form-new-celebration/form-new-celebration.component';
+import { DelayDigitationComponent } from '../../components/delay-digitation/delay-digitation.component';
 
 @Component({
   selector: 'app-main',
-  imports: [FormNewCelebrationComponent],
+  imports: [FormNewCelebrationComponent, DelayDigitationComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })

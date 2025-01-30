c1356f56b18d967033133aad3d4d046c7fa6cede
Franklin <franklinbarbs763@gmail.com>
2025-01-30 15:41:12 -0300

    feat(apiClient): should add localhost api

---
+++ src/app/api/checkout.service.ts
@@ -0,0 +1,43 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+interface CreateNewCelebrationParams {
+  celebrationTitle: string;
+  personName: string;
+  description: string;
+  youtubeUrl: string;
+  endPhrase: string;
+  imageLink: string;
+  email: string;
+  [key: string]: string;
+}
+
+interface CreateNewCelebrationReturn {
+  url: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CheckoutService {
+
+  private apiUrl = `${environment.apiUrl}/payment/checkout-product-base`;
+
+  constructor(private http: HttpClient) {}
+
+  createNewCelebration(params: CreateNewCelebrationParams): Observable<CreateNewCelebrationReturn> {
+    console.log(params)
+    let httpParams = new HttpParams();
+    
+    Object.keys(params).forEach(key => {
+      httpParams = httpParams.set(key, params[key]);
+    });
+  
+    const response: Observable<CreateNewCelebrationReturn> = 
+      this.http.get<CreateNewCelebrationReturn>(this.apiUrl, { params: httpParams });
+    
+    return response;
+  }
+}

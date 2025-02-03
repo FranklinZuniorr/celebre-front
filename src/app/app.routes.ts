8ba29142bc06a4323db931df7b952856af11110a
Franklin <franklinbarbs763@gmail.com>
2025-02-03 13:57:31 -0300

    feat(link-pay): should add new screen to show success payment screen when is completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './screens/main/main.component';
+import { SuccessPayComponent } from './screens/success-pay/success-pay.component';
 
 export const routes: Routes = [
     { path: '', component: MainComponent },
+    { path: 'sucesso', component: SuccessPayComponent }
 ];
